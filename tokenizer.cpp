6a1803032ac839d332374d4f85f559a4eec8c68c
kaiyill <kylestorti@gmail.com>
2024-04-23 01:32:52 -0400

    added in Liams code

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <sstream>
 
 //Plain Commands
 /*
@@ -24,18 +26,138 @@ literal constant, a variable, or an arithmetic expression involving two values (
 which may be literal constants or variables).
 */
 
-int main() {
-    std::string filename = "plainprogram.txt"; // The name of the file to read
-    std::ifstream file(filename); // Create an input file stream object
+enum class TokenType {
+    LINE_NUMBER,
+    COMMAND,
+    VARIABLE,
+    NUMERIC_CONSTANT,
+    OPERATOR,
+    COMMENT
+};
 
-    if (file.is_open()) { // Check if the file opened successfully
-        char c; // A character variable to store each character read from the file
-        while (file.get(c)) { // Read characters from the file one by one
-            std::cout << c; // Print the character to the console. This is where we would have the logic to determine what each char means
+struct Token {
+    TokenType type;
+    std::string value;
+};
+
+class Tokenizer {
+public:
+    Tokenizer(const std::string& filename) : inputFilename(filename) {}
+
+    std::vector<Token> tokenize() {
+        std::vector<Token> tokens;
+        std::ifstream file(inputFilename);
+        if (!file.is_open()) {
+            std::cerr << "Unable to open file: " << inputFilename << std::endl;
+            return tokens;
         }
-        file.close(); // Close the file
-    } else {
-        std::cout << "Unable to open file: " << filename << std::endl;
+
+        std::string line;
+        while (std::getline(file, line)) {
+            if (line.empty()) {
+                continue;
+            }
+            std::istringstream iss(line);
+            std::string word;
+            while (iss >> word) {
+                Token token = classifyToken(word);
+                if (token.type != TokenType::COMMENT) {
+                    tokens.push_back(token);
+                }
+            }
+        }
+        file.close();
+        return tokens;
+    }
+
+private:
+    std::string inputFilename;
+
+    Token classifyToken(const std::string& word) {
+        Token token;
+        if (isLineNumber(word)) {
+            token.type = TokenType::LINE_NUMBER;
+        } else if (isCommand(word)) {
+            token.type = TokenType::COMMAND;
+        } else if (isVariable(word)) {
+            token.type = TokenType::VARIABLE;
+        } else if (isNumericConstant(word)) {
+            token.type = TokenType::NUMERIC_CONSTANT;
+        } else if (isOperator(word)) {
+            token.type = TokenType::OPERATOR;
+        } else if (isComment(word)) {
+            token.type = TokenType::COMMENT;
+        } else {
+            std::cerr << "Unrecognized token: " << word << std::endl;
+            token.type = TokenType::COMMENT;
+        }
+        token.value = word;
+        return token;
+    }
+
+    bool isLineNumber(const std::string& word) {
+        return std::all_of(word.begin(), word.end(), ::isdigit);
+    }
+
+    bool isCommand(const std::string& word) {
+        return (word == "rem" || word == "get" || word == "print" || word == "goto" || word == "if" ||
+                word == "let" || word == "end");
+    }
+
+    bool isVariable(const std::string& word) {
+        return (word.size() == 1 && std::isalpha(word[0]));
+    }
+
+    bool isNumericConstant(const std::string& word) {
+        return std::all_of(word.begin(), word.end(), ::isdigit);
+    }
+
+    bool isOperator(const std::string& word) {
+        return (word == "=" || word == ">");
+    }
+
+    bool isComment(const std::string& word) {
+        return (word == "rem");
+    }
+};
+
+int main() {
+    std::string filename;
+    std::cout << "Enter the name of the file to tokenize";
+    std::getline(std::cin, filename);
+
+    if (filename.empty()) {
+        std::cerr << "No file name." << std::endl;
+        return 1;
+    }
+
+    Tokenizer tokenizer(filename);
+    std::vector<Token> tokens = tokenizer.tokenize();
+
+    std::cout << "Tokens:" << std::endl;
+    for (const auto& token : tokens) {
+        std::cout << "Type: ";
+        switch (token.type) {
+            case TokenType::LINE_NUMBER:
+                std::cout << "LINE_NUMBER";
+                break;
+            case TokenType::COMMAND:
+                std::cout << "COMMAND";
+                break;
+            case TokenType::VARIABLE:
+                std::cout << "VARIABLE";
+                break;
+            case TokenType::NUMERIC_CONSTANT:
+                std::cout << "NUMERIC_CONSTANT";
+                break;
+            case TokenType::OPERATOR:
+                std::cout << "OPERATOR";
+                break;
+            case TokenType::COMMENT:
+                std::cout << "COMMENT";
+                break;
+        }
+        std::cout << " Value " << token.value << std::endl;
     }
 
     return 0;
